71d1ab373e4ce7d4eecca524ae0369b326bdbb20
Olliemint <furywrld5@gmail.com>
2022-06-18 13:45:51 +0300

    feat:profile model

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -6,12 +6,34 @@ from django.contrib.auth.models import User
 class Location(models.Model):
     name = models.CharField(max_length=255)
 
+
+
 class Neighbourhood(models.Model):
     name = models.CharField(max_length=255)
     locattion = models.ForeignKey(Location,on_delete=models.CASCADE)
     occupants_count = models.IntegerField(default=0,blank=True,null=True)
     police_num = models.IntegerField()
     hospital_num = models.IntegerField()
+    
+    
+class Business(models.Model):
+    
+    name = models.CharField(max_length=255)    
+    email_address = models.EmailField(max_length=255)
+    locattion = models.ForeignKey(Location,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=1000)
+    
+    
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_number = models.IntegerField(max_length=50,null=True, blank=True)
+    bio = models.TextField(max_length=1500,null=True,blank=True)
+    email_address = models.EmailField(max_length=255,null=True, blank=True) 
+    avatar = models.ImageField(default='avatar.jpg',upload_to='profiles')   
+    
+    
+    
 
     
 
