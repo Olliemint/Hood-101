1d5abd04fdce7ca7146b9e4ed53ff70e12c989f6
Olliemint <furywrld5@gmail.com>
2022-06-18 13:32:11 +0300

    feat: location,neibourhood and business

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=255)
+    locattion = models.ForeignKey(Location,on_delete=models.CASCADE)
+    occupants_count = models.IntegerField(default=0,blank=True,null=True)
+    police_num = models.IntegerField()
+    hospital_num = models.IntegerField()
+
+    
+
+
+
